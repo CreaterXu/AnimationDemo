955466dffa2fbd8b239ba164e4bdebb5f2a9512b
creater <545262342@qq.com>
2016-06-28 23:12:37 +0800

    添加utils

---
+++ app/src/main/java/com/sky/animationdemo/utils/FileUtils.java
@@ -0,0 +1,43 @@
+package com.sky.animationdemo.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * 文件工具类   主要提供增删改查方法，默认的存储路径为sdcard根目录
+ * Created by Creater Xu on 2016/6/28.
+ *
+ * @version v1.0
+ */
+public class FileUtils {
+    //默认设置  sdcard根目录
+    private static final File root = Environment.getExternalStorageDirectory();
+    //默认的外部存储的路径
+    private static final String mPath = "XuFile";
+
+    public static final int FILE_FORMAT_EXT=1;
+    public static final int FILE_FORMAT_JPG=2;
+
+    /**
+     * 最彻底的保存方法
+     *
+     * @param data,file,path,name,style
+     * data类必须具有tostring()
+     */
+    public static <O> void save(O data, File file, String path, String name, int style) throws ClassNotFoundException {
+        if (file == null)
+            file = root;
+        if (path == null)
+            path = mPath;
+        if (name == null)
+            name = data.toString();
+        if (style == 0)
+            style = FILE_FORMAT_EXT;
+
+        if (data.getClass()==Class.forName(""));
+
+    }
+}
